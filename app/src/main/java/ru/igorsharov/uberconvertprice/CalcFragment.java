4a447f1fd50a6be4dd81a4c77262cdea88050f65
Igor <igorse1984@gmail.com>
2018-03-07 16:52:28 +0300

    внедрен NavigationDrawer, активити переведены на фрагменты

---
+++ app/src/main/java/ru/igorsharov/uberconvertprice/CalcFragment.java
@@ -0,0 +1,245 @@
+package ru.igorsharov.uberconvertprice;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import ru.igorsharov.uberconvertprice.calculation.TariffOne;
+import ru.igorsharov.uberconvertprice.calculation.TariffTwo;
+import ru.igorsharov.uberconvertprice.database.CalcDb;
+
+/**
+ * Created by Игорь on 07.03.2018.
+ */
+
+public class CalcFragment extends Fragment implements View.OnClickListener {
+
+    private StateBox stateBox;
+    private TariffTwo tariffTwo;
+    private TariffOne TariffOne;
+
+    private TextView tvResultOfNewPrice, tvResultOfOldPrice;
+    private TextView tvResultOfNewPrice1, tvResultOfOldPrice1;
+    private Spinner boostSpinner;
+    private CheckBox chBoxOblast, chBoxGarantpik;
+    private Switch switchShowPrice;
+    private LinearLayout oldPrice;
+    private Button btnCls;
+    private CustomEditText etParthnerCommission, etTime, etWay;
+
+    private void initView(View view) {
+        oldPrice = view.findViewById(R.id.oldPrice);
+        view.findViewById(R.id.buttonCalc).setOnClickListener(this);
+        view.findViewById(R.id.buttonClearEditText).setOnClickListener(this);
+        etTime = view.findViewById(R.id.editTextTime);
+        etWay = view.findViewById(R.id.editTextWay);
+        tvResultOfNewPrice = view.findViewById(R.id.textViewNewPrice);
+        tvResultOfOldPrice = view.findViewById(R.id.textViewOldPrice);
+        tvResultOfNewPrice1 = view.findViewById(R.id.textViewNewPrice1);
+        tvResultOfOldPrice1 = view.findViewById(R.id.textViewOldPrice1);
+        boostSpinner = view.findViewById(R.id.boostSpinner);
+        chBoxOblast = view.findViewById(R.id.chbPrigorod);
+        chBoxGarantpik = view.findViewById(R.id.chbGarantpik);
+        switchShowPrice = view.findViewById(R.id.switch2);
+        btnCls = view.findViewById(R.id.buttonClearEditText);
+
+        etParthnerCommission = view.findViewById(R.id.editTextPrthnerCommisson);
+        // фокус на поле "время" при загрузке приложения
+        etTime.requestFocus();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calc, container, false);
+        initView(view);
+
+        // для отслеживания в поле EditText километража свыше заданного
+        listenerHandler();
+        // инициализируем хранилище состояний чекбоксов и вводимых данных из активити
+        stateBox = new StateBox();
+        // инициализируем тарифы
+        tariffTwo = new TariffTwo(stateBox);
+        TariffOne = new TariffOne(stateBox);
+        etParthnerCommission.setText(String.valueOf(tariffTwo.getParthnerComission()));
+        return view;
+    }
+
+
+    // выполнение расчета по нажатию кнопки
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonCalc) {
+            stateBox.setData(getDataOfView(etWay), getDataOfView(etTime), getDataOfView(boostSpinner), getDataOfView(etParthnerCommission));
+            // добавление данных в БД
+            CalcDb.get(getActivity()).addCalc(tariffTwo);
+            calculateAndSetResult();
+            getSnackbar(view, "Посчитано", R.color.colorGreen).show();
+        } else {
+            getSnackbar(view, "Очищено", R.color.colorAccent).show();
+            clsView();
+        }
+    }
+
+
+    private Snackbar getSnackbar(View view, String msg, int color) {
+        Snackbar snackbar = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
+        View snackBarView = snackbar.getView();
+        //TODO не нравится способ получения контекста, да и под вопросом способ получения цвета из ресурсов
+        snackBarView.setBackgroundColor(ContextCompat.getColor(getActivity(), color));
+        return snackbar;
+    }
+
+    private void clsView() {
+        // очистка полей ввода
+        clearViewText(etTime);
+        clearViewText(etWay);
+        clearViewText(tvResultOfOldPrice);
+        clearViewText(tvResultOfNewPrice);
+        clearViewText(tvResultOfOldPrice1);
+        clearViewText(tvResultOfNewPrice1);
+        etTime.requestFocus();
+        boostSpinner.setSelection(0);
+    }
+
+
+    private void clearViewText(TextView view) {
+        view.setText("");
+    }
+
+    private void listenerHandler() {
+        // слушаем поле EditText расстояние
+        etWay.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (getDataOfView(etWay) > tariffTwo.getKmOver()) {
+                    chBoxOblast.setVisibility(View.VISIBLE);
+                } else {
+                    chBoxOblast.setChecked(false);
+                    chBoxOblast.setVisibility(View.GONE);
+                }
+
+                // отслеживание пустотности EditText
+                if (String.valueOf(etWay.getText()).equals("") && String.valueOf(etTime.getText()).equals(""))
+                    btnCls.setVisibility(View.GONE);
+                else
+                    btnCls.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // слушаем поле EditText время
+        etTime.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // отслеживание пустотности EditText
+                if (String.valueOf(etWay.getText()).equals("") && String.valueOf(etTime.getText()).equals(""))
+                    btnCls.setVisibility(View.GONE);
+                else
+                    btnCls.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // слушаем spinner
+        boostSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i > 0) {
+                    chBoxGarantpik.setVisibility(View.VISIBLE);
+                } else {
+                    chBoxGarantpik.setChecked(false);
+                    chBoxGarantpik.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // TODO оптимизировать чекбоксы, сделать имплемент интерфейса и свитч
+        // слушаем чекбоксы
+        chBoxOblast.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                stateBox.setChBoxOblastState(b);
+            }
+        });
+
+        chBoxGarantpik.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                stateBox.setChBoxGarantpikState(b);
+            }
+        });
+
+        // переключатель видимости старого тарифа
+        switchShowPrice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                oldPrice.setVisibility(b ? View.VISIBLE : View.GONE);
+            }
+        });
+
+    }
+
+
+    // общий метод получения данных из полей ввода
+    private float getDataOfView(View v) {
+        if (v instanceof TextView) {
+            TextView tv = (TextView) v;
+            if (!tv.getText().toString().equals("")) {
+                return Float.parseFloat(tv.getText().toString());
+            }
+        } else if (v instanceof Spinner) {
+            Spinner sp = (Spinner) v;
+            return Float.parseFloat((String) sp.getSelectedItem());
+        }
+        return 0;
+    }
+
+
+    private void calculateAndSetResult() {
+        setResults(tvResultOfNewPrice, tariffTwo.getCost());
+        setResults(tvResultOfNewPrice1, tariffTwo.getProfit());
+        setResults(tvResultOfOldPrice, TariffOne.getCost());
+        setResults(tvResultOfOldPrice1, TariffOne.getProfit());
+    }
+
+
+    private void setResults(TextView tv, float priceTotal) {
+        tv.setText(String.valueOf((int) priceTotal).concat(" руб."));
+    }
+
+}
--- app/src/main/java/ru/igorsharov/uberconvertprice/MainActivity.java
+++ app/src/main/java/ru/igorsharov/uberconvertprice/MainActivity.java
@@ -1,241 +1,144 @@
 package ru.igorsharov.uberconvertprice;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import ru.igorsharov.uberconvertprice.calculation.TariffOne;
-import ru.igorsharov.uberconvertprice.calculation.TariffTwo;
-import ru.igorsharov.uberconvertprice.database.CalcDb;
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private TextView tvResultOfNewPrice, tvResultOfOldPrice;
-    private TextView tvResultOfNewPrice1, tvResultOfOldPrice1;
-    private Spinner boostSpinner;
-    private CheckBox chBoxOblast, chBoxGarantpik;
-    private Switch switchShowPrice;
-    private LinearLayout oldPrice;
-    private StateBox stateBox;
-    private TariffTwo tariffTwo;
-    private TariffOne TariffOne;
-    private Button btnCls;
-    private CustomEditText etParthnerCommission, etTime, etWay;
-
-    private void initView() {
-        oldPrice = findViewById(R.id.oldPrice);
-        findViewById(R.id.buttonCalc).setOnClickListener(this);
-        findViewById(R.id.buttonClearEditText).setOnClickListener(this);
-        etTime = findViewById(R.id.editTextTime);
-        etWay = findViewById(R.id.editTextWay);
-        tvResultOfNewPrice = findViewById(R.id.textViewNewPrice);
-        tvResultOfOldPrice = findViewById(R.id.textViewOldPrice);
-        tvResultOfNewPrice1 = findViewById(R.id.textViewNewPrice1);
-        tvResultOfOldPrice1 = findViewById(R.id.textViewOldPrice1);
-        boostSpinner = findViewById(R.id.boostSpinner);
-        chBoxOblast = findViewById(R.id.chbPrigorod);
-        chBoxGarantpik = findViewById(R.id.chbGarantpik);
-        switchShowPrice = findViewById(R.id.switch2);
-        btnCls = findViewById(R.id.buttonClearEditText);
-
-        etParthnerCommission = findViewById(R.id.editTextPrthnerCommisson);
-        // фокус на времени при загрузке приложения
-        etTime.requestFocus();
-    }
+    CalcFragment calcFragment = new CalcFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
 
-        // для отслеживания в поле EditText километража свыше заданного
-        listenerHandler();
-        // инициализируем хранилище состояний чекбоксов и вводимых данных из активити
-        stateBox = new StateBox();
-        // инициализируем тарифы
-        tariffTwo = new TariffTwo(stateBox);
-        TariffOne = new TariffOne(stateBox);
-        etParthnerCommission.setText(String.valueOf(tariffTwo.getParthnerComission()));
+        // делает ActionBar активным (появляется текст с названием проекта)
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        // TODO переделать на добавление расчета в БД
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        // инициализация гамбургера на тулбаре
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        // вешаем слушателя на drawer
+        drawer.addDrawerListener(toggle);
+
+        // синхронизирует hamburger menu с drawer
+        // без данной строки кнопка гамбургер не появится
+        toggle.syncState();
+
+        // вешаем листнер на пункты меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        getFragmentManager().beginTransaction().add(R.id.container, calcFragment).commit();
     }
 
 
-    // выполнение расчета по нажатию кнопки
     @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.buttonCalc) {
-            stateBox.setData(getDataOfView(etWay), getDataOfView(etTime), getDataOfView(boostSpinner), getDataOfView(etParthnerCommission));
-            // добавление данных в БД
-            CalcDb.get(getApplicationContext()).addCalc(tariffTwo);
-            calculateAndSetResult();
-            getSnackbar(view, "Посчитано", R.color.colorGreen).show();
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
         } else {
-            getSnackbar(view, "Очищено", R.color.colorAccent).show();
-            clsView();
+            super.onBackPressed();
         }
     }
 
-
-    private Snackbar getSnackbar(View view, String msg, int color) {
-        Snackbar snackbar = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
-        View snackBarView = snackbar.getView();
-        //TODO не нравится способ получения контекста, да и под вопросом способ получения цвета из ресурсов
-        snackBarView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), color));
-        return snackbar;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    private void clsView() {
-        // очистка полей ввода
-        clearViewText(etTime);
-        clearViewText(etWay);
-        clearViewText(tvResultOfOldPrice);
-        clearViewText(tvResultOfNewPrice);
-        clearViewText(tvResultOfOldPrice1);
-        clearViewText(tvResultOfNewPrice1);
-        etTime.requestFocus();
-        boostSpinner.setSelection(0);
-    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-
-    private void clearViewText(TextView view) {
-        view.setText("");
-    }
-
-    private void listenerHandler() {
-        // слушаем поле EditText расстояние
-        etWay.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (getDataOfView(etWay) > tariffTwo.getKmOver()) {
-                    chBoxOblast.setVisibility(View.VISIBLE);
-                } else {
-                    chBoxOblast.setChecked(false);
-                    chBoxOblast.setVisibility(View.GONE);
-                }
-
-                // отслеживание пустотности EditText
-                if (String.valueOf(etWay.getText()).equals("") && String.valueOf(etTime.getText()).equals(""))
-                    btnCls.setVisibility(View.GONE);
-                else
-                    btnCls.setVisibility(View.VISIBLE);
-            }
-        });
-
-        // слушаем поле EditText время
-        etTime.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // отслеживание пустотности EditText
-                if (String.valueOf(etWay.getText()).equals("") && String.valueOf(etTime.getText()).equals(""))
-                    btnCls.setVisibility(View.GONE);
-                else
-                    btnCls.setVisibility(View.VISIBLE);
-            }
-        });
-
-        // слушаем spinner
-        boostSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i > 0) {
-                    chBoxGarantpik.setVisibility(View.VISIBLE);
-                } else {
-                    chBoxGarantpik.setChecked(false);
-                    chBoxGarantpik.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        // TODO оптимизировать чекбоксы, сделать имплемент интерфейса и свитч
-        // слушаем чекбоксы
-        chBoxOblast.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                stateBox.setChBoxOblastState(b);
-            }
-        });
-
-        chBoxGarantpik.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                stateBox.setChBoxGarantpikState(b);
-            }
-        });
-
-        // переключатель видимости старого тарифа
-        switchShowPrice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                oldPrice.setVisibility(b ? View.VISIBLE : View.GONE);
-            }
-        });
-
-    }
-
-
-    // общий метод получения данных из полей ввода
-    private float getDataOfView(View v) {
-        if (v instanceof TextView) {
-            TextView tv = (TextView) v;
-            if (!tv.getText().toString().equals("")) {
-                return Float.parseFloat(tv.getText().toString());
-            }
-        } else if (v instanceof Spinner) {
-            Spinner sp = (Spinner) v;
-            return Float.parseFloat((String) sp.getSelectedItem());
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        return 0;
+
+        return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Создадим новый фрагмент
+        Fragment fragment = null;
+        Class fragmentClass = null;
 
-    private void calculateAndSetResult() {
-        setResults(tvResultOfNewPrice, tariffTwo.getCost());
-        setResults(tvResultOfNewPrice1, tariffTwo.getProfit());
-        setResults(tvResultOfOldPrice, TariffOne.getCost());
-        setResults(tvResultOfOldPrice1, TariffOne.getProfit());
-    }
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
 
-    private void setResults(TextView tv, float priceTotal) {
-        tv.setText(String.valueOf((int) priceTotal).concat(" руб."));
+        switch (id) {
+            case R.id.nav_calc:
+                fragmentClass = CalcFragment.class;
+                Toast.makeText(getApplicationContext(),
+                        "Вы выбрали 1",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_statistic:
+                fragmentClass = TestFragment.class;
+                Toast.makeText(getApplicationContext(),
+                        "Вы выбрали 2",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Вставляем фрагмент, заменяя текущий фрагмент
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+        // Выделяем выбранный пункт меню в шторке
+        item.setChecked(true);
+        // Выводим выбранный пункт в заголовке
+        setTitle(item.getTitle());
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 // Способ скрывать клавиатуру с экрана
